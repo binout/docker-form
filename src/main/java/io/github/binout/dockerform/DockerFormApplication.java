2e7005c46098f464298cc08a9bfd68a52b5341e5
b.prioux <b.prioux@lectra.com>
2017-06-07 13:38:28 +0200

    init java project

---
+++ src/main/java/io/github/binout/dockerform/DockerFormApplication.java
@@ -0,0 +1,12 @@
+package io.github.binout.dockerform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerFormApplication.class, args);
+	}
+}
